e9ed48bab338598ff154ceb6d85fb06905ed38e0
c.giacometti <camilla.giacometti@gmail.com>
2022-10-01 16:40:55 -0300

    feat: login controller]

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
+import * as userService from "../services/userService";
 
 export async function login(req: Request, res: Response) {
 
-    const userData = req.body;
+    const { email, password } = req.body;
+
+    const { token } = await userService.loginUser(email, password);
     
+    return res.status(200).send({ token });
+
 }
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { login }from "../controllers/authController";
 
 const router = Router();
 
-router.post("/signin");
+router.post("/signin", login);
 
 export default router;
